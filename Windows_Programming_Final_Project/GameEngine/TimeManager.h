1c67bff93957fb6b62446f1240703eeb4c0ae434
KangTaeYang <taeyang0311@gmail.com>
2026-05-15 23:10:24 +0900

    InputManager, TimeManager 작성 pch.h 수

--- Windows_Programming_Final_Project/GameEngine/TimeManager.h
+++ Windows_Programming_Final_Project/GameEngine/TimeManager.h
@@ -4,11 +4,23 @@
 
 class TimeManager
 {
-	TimeManager();
-	~TimeManager();
+	DECLARE_SINGLE(TimeManager);
 
+public:
+	void Init();
+	void Update();
 
+	unsigned int GetFps() { return _fps; }
+	float GetDeltaTime() { return _deltaTime; }
 
-	int deltaTime{}, prevTime{};
+private:
+	unsigned __int64 _frequency = 0;
+	unsigned __int64 _prevCount = 0;
+	float _deltaTime = 0.f;
+
+private:
+	unsigned int _frameCount = 0;
+	float _frameTime = 0.f;
+	unsigned int _fps = 0;
 };
 
