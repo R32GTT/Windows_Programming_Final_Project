ec79576664ae780e87508ebbd88ecda4ba62c349
DonSooKang <ehdth9191@naver.com>
2026-06-04 00:08:34 +0900

    Deco도 살짝 수정함

--- Windows_Programming_Final_Project/GameEngine/Objects/Deco.cpp
+++ Windows_Programming_Final_Project/GameEngine/Objects/Deco.cpp
@@ -6,16 +6,36 @@ DECO::~DECO()
 {
 }
 
+//_halfSize는 wall과 일단 똑같이 25 25
 void DECO::Init()
 {
-
-
+	type = OBJECTTYPE::DECO;
+	_halfSize = { 25.0f, 25.0f };
 }
 
 void DECO::Update()
 {
+
+
 }
 
 void DECO::Render(ID2D1RenderTarget* renderTarget, float alpha)
 {
+	if (renderTarget == nullptr) return;
+
+	Vec2<float> renderPos = GetRenderPos(alpha);
+
+	D2D1_RECT_F rect = D2D1::RectF(renderPos.x - _halfSize.x, renderPos.y - _halfSize.y, renderPos.x + _halfSize.x, renderPos.y + _halfSize.y);
+
+	ID2D1SolidColorBrush* brush = nullptr;
+
+	renderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightGreen), &brush);
+
+	if (brush != nullptr)
+	{
+		renderTarget->FillRectangle(rect, brush);
+		brush->Release();
+	}
+
+
 }
