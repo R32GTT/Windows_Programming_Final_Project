d1de60b08189b6ddf0a6b68c926885cff34454af
DonSooKang <ehdth9191@naver.com>
2026-06-07 16:58:39 +0900

    Projectile.h .cpp 수정

--- Windows_Programming_Final_Project/GameEngine/Objects/Projectile.cpp
+++ Windows_Programming_Final_Project/GameEngine/Objects/Projectile.cpp
@@ -11,6 +11,59 @@ Projectile::~Projectile()
 {
 }
 
+//Projectile.cpp 함수들 추가
+void Projectile::Init() {
+
+	speed = 500.0f;
+	_halfSize = { 10.0f, 10.0f };
+
+}
+
+void Projectile::Update()
+{
+	//60fps라고 가정하고 고정 시간으로 움직이는 방식
+	float deltaTime = 0.016f;
+
+	prevPos = pos;
+
+	pos = pos + _dir * speed * deltaTime;
+
+	_elapsedTime += deltaTime;
+
+}
+
+void Projectile::Render(ID2D1RenderTarget* renderTarget, float alpha)
+{
+	Vec2<float> renderPos = GetRenderPos(alpha);
+
+	D2D1_ELLIPSE ellipse = D2D1::Ellipse(D2D1::Point2F(renderPos.x, renderPos.y), _halfSize.x, _halfSize.y);
+
+	ID2D1SolidColorBrush* brush = nullptr;
+
+	renderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Yellow), &brush);
+
+	renderTarget->FillEllipse(ellipse, brush);
+
+	if (brush != nullptr)
+		brush->Release();
+
+}
+
+bool Projectile::CheckDead()
+{
+	return _elapsedTime >= _lifeTime;
+}
+
+void Projectile::SetDirection(Vec2<float> dir)
+{
+	_dir = dir.Normalized();
+}
+
+void Projectile::SetOwner(GameObject* owner)
+{
+	_owner = owner;
+}
+
 void Projectile::SaveToData(ObjectSpawnData& outData)
 {
 }
--- Windows_Programming_Final_Project/GameEngine/Objects/Projectile.h
+++ Windows_Programming_Final_Project/GameEngine/Objects/Projectile.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "pch.h"
 #include "GameObject.h"
+#include "Enums.h"
 
 class Projectile : public GameObject
 {
@@ -7,7 +9,34 @@ public:
 	Projectile();
 	virtual ~Projectile();
 
+	//Projectile 만들기 시작
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render(ID2D1RenderTarget* renderTarget, float alpha) override;
+	virtual bool CheckDead() override;
+
+	void SetDirection(Vec2<float> dir);
+
+	//데미지는 한 방 컷이어서 일단 뺌
+	//필요하면 추가하겠음 
+	//void SetDamage(int damage);
+	
+	//owner의 의미는 이 Projectile을 누가 발사했는지를
+	//기억하는 포인터이다
+	void SetOwner(GameObject* owner);
+
 	virtual void SaveToData(ObjectSpawnData& outData) override;
 	virtual void LoadFromData(const ObjectSpawnData& spawnData) override;
+
+private:
+
+	//private도 추가함
+	Vec2<float> _dir{};
+	int _damage = 10;
+
+	float _lifeTime = 2.0f;
+	float _elapsedTime = 0.0f;
+
+	GameObject* _owner = nullptr;
 
 };
