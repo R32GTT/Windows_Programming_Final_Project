d903c1e1a94cd37c28f895916542c9a7f90d08a7
DonSooKang <ehdth9191@naver.com>
2026-06-10 20:19:54 +0900

    무기 스폰 만들기 시작

--- Windows_Programming_Final_Project/GameEngine/Objects/Weapon.cpp
+++ Windows_Programming_Final_Project/GameEngine/Objects/Weapon.cpp
@@ -20,13 +20,32 @@ void Weapon::Update()
 {
 	if (owner != nullptr)
 	{
-
+		this->pos = owner->GetPos();
 	}
 
 }
 
 void Weapon::Render(ID2D1RenderTarget* renderTarget, float alpha)
 {
+	if (!renderTarget) return;
+
+	Vec2<float> renderPos = GetRenderPos(alpha);
+
+	//임시로 가로 20 세로 10 크기의 사각형(총 모양처럼) 그려본다
+	D2D1_RECT_F rect = D2D1::RectF(renderPos.x - 10.0f, renderPos.y - 5.0f, renderPos.x + 10.0f, renderPos.y + 5.0f);
+
+	ID2D1SolidColorBrush* brush = nullptr;
+
+	renderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Orange), &brush);
+
+	if (brush != nullptr)
+	{
+		renderTarget->FillRectangle(rect, brush);
+		brush->Release();
+	}
+
+
+
 }
 
 void Weapon::SaveToData(ObjectSpawnData& outData)
